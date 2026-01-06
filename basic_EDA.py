ac71b201aee366394e0c0002f26565ec7555c0bb
Riya Choudhary <riiyachoudhary9@gmail.com>
2026-01-06 13:43:24 +0530

    Update and rename pypy.py to basic_EDA.py

--- basic_EDA.py
+++ basic_EDA.py
@@ -1,15 +1,11 @@
 #Exp. 7 Write a program in Python to perform basic exploratory data analysis
-(EDA).
-Aim: To conduct basic EDA for understanding the structure and summary of a dataset.
-Objectives:
-●
-●
-●
-●
-Summarize data using .describe() and .info().
-Check data types, unique values, and distributions.
-Identify patterns, outliers, or trends.
-Build foundational insights for advanced analysis.
+#(EDA).
+#Aim: To conduct basic EDA for understanding the structure and summary of a dataset.
+#Objectives:
+#Summarize data using .describe() and .info().
+# Check data types, unique values, and distributions.
+# Identify patterns, outliers, or trends.
+# Build foundational insights for advanced analysis.
 
 
 
